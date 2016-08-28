b272d28b65f5172cff76e3c4f41c8cdd3d386fe6
Lee Han Cheng <lhchchc@gmail.com>
2016-08-28 15:43:36 +0800

    Add getter for feedbackToUser

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -27,6 +27,13 @@ public class CommandResult {
     }
 
     /**
+	 * @return the feedbackToUser
+	 */
+	public String getFeedbackToUser() {
+		return feedbackToUser;
+	}
+
+	/**
      * Returns list of persons relevant to the command command result, if any.
      */
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
