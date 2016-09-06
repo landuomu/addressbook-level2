943d12412ffd98b28f39fdd881d5b38e917d538d
Lee Han Cheng <lhchchc@gmail.com>
2016-09-07 02:15:39 +0800

    Add JUnit tests for Utils.elementsAreUnique method

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 public class UtilsTest {
 	
 	/**
@@ -25,4 +28,32 @@ public class UtilsTest {
 		assertTrue(Utils.isAnyNull(1,2,null));
 	}
 
+	/**
+	 * Test elementsAreUnique method
+	 */
+	
+	@Test
+	public void elementsAreUnique_emptyCollection() {
+		Collection<Object> emptyList = new ArrayList<Object>();
+		assertTrue(Utils.elementsAreUnique(emptyList));
+	}
+	
+	@Test
+	public void elementsAreUnique_uniqueCollection() {
+		Collection<Object> uniqueList = new ArrayList<Object>();
+		uniqueList.add(1);
+		uniqueList.add("a");
+		uniqueList.add(false);
+		assertTrue(Utils.elementsAreUnique(uniqueList));
+	}
+	
+	@Test
+	public void elementsAreUnique_duplicateCollection() {
+		Collection<Object> duplicateList = new ArrayList<Object>();
+		duplicateList.add(1);
+		duplicateList.add("a");
+		duplicateList.add("a");
+		assertFalse(Utils.elementsAreUnique(duplicateList));
+	}
+	
 }
